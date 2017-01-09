2edc01e1189e8d2fc9cfe93c81763e9d31762ac3
Homa Wong <homawong@gmail.com>
2017-01-08 22:11:35 -0800

    init commit

---
+++ src/GlobalStore.spec.ts
@@ -0,0 +1,30 @@
+import test from 'ava'
+
+import { getLogger, Logger, Appender } from 'aurelia-logging'
+
+import { getStore, removeStore } from './index'
+
+const logger = getLogger('GlobalStore:spec')
+test('simple string store', t => {
+  const store = getStore('something')
+  store.value = 'somevalue'
+
+  t.is(store.value, 'somevalue')
+
+  t.is(getStore('something', ''), store)
+
+  removeStore(store)
+  const store2 = getStore('something')
+  t.not(store2, store)
+
+  t.notThrows(() => removeStore(undefined as any))
+})
+
+test('complex store', t => {
+  const defaultValue = { loggers: [] as Logger[], appenders: [] as Appender[] }
+  const store = getStore('aurelia-logging:global', defaultValue)
+  store.value.loggers.push(logger)
+
+  const another = getStore('aurelia-logging:global', defaultValue)
+  t.is(another.value.loggers[0], logger)
+})
---
+++ src/GlobalStore.ts
@@ -0,0 +1,58 @@
+
+/**
+ * Store interface.
+ */
+export interface Store<T> {
+  /**
+   * ID of the store.
+   */
+  id: string
+
+  /**
+   * The store value.
+   */
+  value: T
+}
+
+/**
+ * Store implementation.
+ * The Store interface and StoreImpl is separated so that
+ * consumer cannot get access to the StoreImpl class to instantiate it directly.
+ */
+class StoreImpl<T> implements Store<T> {
+  value: T
+
+  /**
+   * `defaultValue` is default to `{}` to support simple hash.
+   * If consumer use this is simple type store (`string`, `number` etc),
+   * this `{}` is wasted but it is a trade off to be made.
+   */
+  constructor(public id: string, defaultValue: T = {} as T) {
+    this.value = defaultValue
+   }
+}
+
+const globalState: { [i: string]: Store<any> } = {}
+
+/**
+ * Gets or creates a store.
+ * @param id A unique identifier to the store.
+ * This id MUST be unique across all modules in an application.
+ * You should make it descriptive.
+ * You should use your module's name or unique key as part of the id.
+ * @param defaultValue Optional, but most of the time you will specify it.
+ * You can skip the defaultValue for very basic use cases: basic types and hash.
+ */
+export function getStore<T>(id: string, defaultValue?: T): Store<T> {
+  return globalState[id] = globalState[id] || new StoreImpl<T>(id, defaultValue)
+}
+
+/**
+ * Remove a store from the global state.
+ * You normally would not do this as your module (across all versions) depends on the store.
+ */
+export function removeStore(store: Store<any>): void {
+  if (store) {
+    delete globalState[store.id]
+  }
+}
