a6353332e4483109986206f6112c26c7eb3abcf4
Homa Wong <homawong@gmail.com>
2017-01-12 12:32:24 -0800

    Update to use rollup

---
+++ rollup.config.global.js
@@ -0,0 +1,29 @@
+import paramCase from 'param-case';
+import pascalCase from 'pascal-case';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeBuiltins from 'rollup-plugin-node-builtins';
+import nodeGlobals from 'rollup-plugin-node-globals';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import uglify from 'rollup-plugin-uglify';
+
+const pkg = require('./package');
+
+const moduleName = pascalCase(pkg.name)
+
+export default {
+  moduleId: pkg.name,
+  moduleName,
+  entry: 'dist/commonjs/index.js',
+  dest: `dist/${paramCase(pkg.name)}.js`,
+  format: 'iife',
+  sourceMap: true,
+  plugins: [
+    sourcemaps(),
+    nodeResolve({ jsnext: true }),
+    nodeGlobals(),
+    nodeBuiltins(),
+    commonjs(),
+    uglify()
+  ]
+};
