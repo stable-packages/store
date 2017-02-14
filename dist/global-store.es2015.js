f87a14f2debc32c18d2ca15511bafa40a5aadef1
Homa Wong <homawong@gmail.com>
2017-02-14 01:03:19 -0800

    0.6.0

--- dist/global-store.es2015.js
+++ dist/global-store.es2015.js
@@ -1,2 +1,2 @@
-!function(t){"use strict";function e(t,e){return n[t]=n[t]||new i(t,e)}class i{constructor(t,e={}){this.id=t,this.value=e}}const n={};t.default=e,t.getStore=e}(this.GlobalStore=this.GlobalStore||{});
+!function(t){"use strict";function n(t,n){return e[t]=e[t]||n}function o(t,n){e[t]=n}function u(t,u){return{get(){return n(t,u)},set(n){o(t,n)}}}const e={};t.get=n,t.set=o,t.default=u}(this.GlobalStore=this.GlobalStore||{});
 //# sourceMappingURL=global-store.es2015.js.map
