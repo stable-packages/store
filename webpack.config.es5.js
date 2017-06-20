ca27a754e2999d4df0c9d4a9bfb24c4067e66ba5
Homa Wong <homawong@gmail.com>
2017-06-19 19:27:02 -0700

    Remove uglify

--- webpack.config.es5.js
+++ webpack.config.es5.js
@@ -2,7 +2,6 @@
 const paramCase = require('param-case')
 const pascalCase = require('pascal-case')
 const path = require('path')
-const webpack = require('webpack')
 
 const pjson = require('./package.json')
 
@@ -36,12 +35,5 @@ module.exports = {
         return `webpack:///${info.resourcePath}`
       }
     }
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: false },
-      sourceMap: true
-    })
-  ]
+  }
 }
