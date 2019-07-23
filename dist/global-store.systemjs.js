991b714076a8b0e8956e7651e9eede13d0766b61
Homa Wong (unional) <homawong@gmail.com>
2019-07-23 01:58:08 -0700

    v1.0.0-beta.5

--- dist/global-store.systemjs.js
+++ dist/global-store.systemjs.js
@@ -4,9 +4,11 @@ System.register([], function (exports) {
         execute: function () {
 
             exports({
+                createAsyncStore: createAsyncStore,
                 createReadonlyStore: createReadonlyStore,
                 createStore: createStore,
-                default: createStore
+                default: createStore,
+                initializeAsyncStore: initializeAsyncStore
             });
 
             /*! *****************************************************************************
@@ -49,6 +51,146 @@ System.register([], function (exports) {
                 return __assign.apply(this, arguments);
             };
 
+            function __awaiter(thisArg, _arguments, P, generator) {
+                return new (P || (P = Promise))(function (resolve, reject) {
+                    function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+                    function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+                    function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+                    step((generator = generator.apply(thisArg, _arguments || [])).next());
+                });
+            }
+
+            function __generator(thisArg, body) {
+                var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+                return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+                function verb(n) { return function (v) { return step([n, v]); }; }
+                function step(op) {
+                    if (f) throw new TypeError("Generator is already executing.");
+                    while (_) try {
+                        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+                        if (y = 0, t) op = [op[0] & 2, t.value];
+                        switch (op[0]) {
+                            case 0: case 1: t = op; break;
+                            case 4: _.label++; return { value: op[1], done: false };
+                            case 5: _.label++; y = op[1]; op = [0]; continue;
+                            case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                            default:
+                                if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                                if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                                if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                                if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                                if (t[2]) _.ops.pop();
+                                _.trys.pop(); continue;
+                        }
+                        op = body.call(thisArg, _);
+                    } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+                    if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+                }
+            }
+
+            function getStoreValue(stores, id) {
+                var moduleStore = getModuleStore(stores, id.moduleName);
+                return moduleStore[id.key].value;
+            }
+            function initStoreValue(stores, id, initializer) {
+                var moduleStore = getModuleStore(stores, id.moduleName);
+                var store = moduleStore[id.key];
+                var init = initializer(store && store.init || {});
+                moduleStore[id.key] = { init: init, value: createStoreValue(init) };
+            }
+            function resetStoreValue(stores, id) {
+                var moduleStore = getModuleStore(stores, id.moduleName);
+                moduleStore[id.key].value = createStoreValue(moduleStore[id.key].init);
+            }
+            function getModuleStore(stores, moduleName) {
+                return stores[moduleName] = stores[moduleName] || {};
+            }
+            function createStoreValue(initialValue) {
+                return __assign({}, initialValue);
+            }
+
+            var stores = {};
+            /**
+             * Creates a store of type T.
+             * @param moduleName Name of your module. This will be used during reporting.
+             * @param key Specific key of the store scoped to your module. This will not appear in reporting.
+             * You can use `Symbol.for(<some key>)` to make the store accessible accross service workers and iframes.
+             *
+             * It is recommend that the key contains the purpose as well as a random value such as GUID.
+             * e.g. `some-purpose:c0574313-5f6c-4c02-a875-ad793d47b695`
+             * This key should not change across versions.
+             * @param initializer Initializing function for the store
+             */
+            function createStore(moduleName, key, initializer) {
+                initStoreValue(stores, { moduleName: moduleName, key: key }, initializer);
+                return {
+                    get: function () { return getStoreValue(stores, { moduleName: moduleName, key: key }); },
+                    reset: function () { return resetStoreValue(stores, { moduleName: moduleName, key: key }); }
+                };
+            }
+
+            var asyncStoreCreators = {};
+            /**
+             * Creates a store of type T asychronously.
+             * @param moduleName Name of your module. This will be used during reporting.
+             * @param key Specific key of the store scoped to your module. This will not appear in reporting.
+             * You can use `Symbol.for(<some key>)` to make the store accessible accross service workers and iframes.
+             *
+             * It is recommend that the key contains the purpose as well as a random value such as GUID.
+             * e.g. `some-purpose:c0574313-5f6c-4c02-a875-ad793d47b695`
+             * This key should not change across versions.
+             * @param version Version of the store. It can be numeric or string in the format of "major.minor.patch".
+             * No other string format is accepted.
+             * When it is numeric, it is compare to the patch number of the string version,
+             * if there is a mix of number and string versions.
+             * @param initializer Initializing function for the store
+             */
+            function createAsyncStore(moduleName, key, version, initializer) {
+                return __awaiter(this, void 0, Promise, function () {
+                    return __generator(this, function (_a) {
+                        return [2 /*return*/, new Promise(function (resolve) {
+                                var creatorsOfModules = asyncStoreCreators[moduleName] = asyncStoreCreators[moduleName] || {};
+                                var storeCreators = creatorsOfModules[key] = creatorsOfModules[key] || [];
+                                storeCreators.push({ version: version, resolve: resolve, initializer: initializer });
+                            })];
+                    });
+                });
+            }
+            /**
+             * Initializes the stores for `createAsyncStore()`.
+             */
+            function initializeAsyncStore(moduleName, key) {
+                var creatorsOfModules = asyncStoreCreators[moduleName];
+                if (!creatorsOfModules)
+                    return;
+                if (!key) {
+                    Object.keys(creatorsOfModules).forEach(function (k) {
+                        var storeCreators = creatorsOfModules[k];
+                        resolveCreators(moduleName, k, storeCreators);
+                    });
+                }
+                else {
+                    var storeCreators = creatorsOfModules[key];
+                    resolveCreators(moduleName, key, storeCreators);
+                }
+            }
+            function resolveCreators(moduleName, key, storeCreators) {
+                sortByVersion(storeCreators).forEach(function (_a) {
+                    var resolve = _a.resolve, initializer = _a.initializer;
+                    return resolve(createStore(moduleName, key, initializer));
+                });
+            }
+            function sortByVersion(storeCreators) {
+                return storeCreators.sort(function (a, b) { return compareVersion(typeof a.version === 'number' ? "0.0." + a.version : a.version, typeof b.version === 'number' ? "0.0." + b.version : b.version); });
+            }
+            function compareVersion(a, b) {
+                var v1 = a.split('.').map(function (v) { return Number(v); });
+                var v2 = b.split('.').map(function (v) { return Number(v); });
+                return v1[0] !== v2[0] ? v1[0] - v2[0] :
+                    v1[1] !== v2[1] ? v1[1] - v2[1] :
+                        v1[2] - v2[2];
+            }
+
             var Prohibited = exports('Prohibited', /** @class */ (function (_super) {
                 __extends(Prohibited, _super);
                 function Prohibited(moduleName, action) {
@@ -72,27 +214,6 @@ System.register([], function (exports) {
                 }
                 return AccessedBeforeLock;
             }(Error)));
-
-            function getStoreValue(stores, id) {
-                var moduleStore = getModuleStore(stores, id.moduleName);
-                return moduleStore[id.key].value;
-            }
-            function initStoreValue(stores, id, initializer) {
-                var moduleStore = getModuleStore(stores, id.moduleName);
-                var store = moduleStore[id.key];
-                var init = initializer(store && store.init || {});
-                moduleStore[id.key] = { init: init, value: createStoreValue(init) };
-            }
-            function resetStoreValue(stores, id) {
-                var moduleStore = getModuleStore(stores, id.moduleName);
-                moduleStore[id.key].value = createStoreValue(moduleStore[id.key].init);
-            }
-            function getModuleStore(stores, moduleName) {
-                return stores[moduleName] = stores[moduleName] || {};
-            }
-            function createStoreValue(initialValue) {
-                return __assign({}, initialValue);
-            }
 
             var readonlyStores = {};
             /**
@@ -170,26 +291,6 @@ System.register([], function (exports) {
                 if (Array.isArray(value)) {
                     storeValue[k] = Object.freeze(value);
                 }
-            }
-
-            var stores = {};
-            /**
-             * Creates a store of type T.
-             * @param moduleName Name of your module. This will be used during reporting.
-             * @param key Specific key of the store scoped to your module. This will not appear in reporting.
-             * You can use `Symbol.for(<some key>)` to make the store accessible accross service workers and iframes.
-             *
-             * It is recommend that the key contains the purpose as well as a random value such as GUID.
-             * e.g. `some-purpose:c0574313-5f6c-4c02-a875-ad793d47b695`
-             * This key should not change across versions.
-             * @param initializer Initializing function for the store
-             */
-            function createStore(moduleName, key, initializer) {
-                initStoreValue(stores, { moduleName: moduleName, key: key }, initializer);
-                return {
-                    get: function () { return getStoreValue(stores, { moduleName: moduleName, key: key }); },
-                    reset: function () { return resetStoreValue(stores, { moduleName: moduleName, key: key }); }
-                };
             }
 
         }
