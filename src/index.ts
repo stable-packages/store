b680c9cc6caae7010edb76a7e5580cb942f83770
Homa Wong <homawong@gmail.com>
2019-08-03 14:41:04 -0700

    feat: add `freeze()` to `Store` (#69)
    * feat: add `freeze()` to `Store`
    
    BREAKING CHANGE: remove ReadonlyStore
    
    The protection feature in ReadonlyStore is sufficiently provided by `freeze()`.
    
    There is no need to increate the complexity of the library.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 export * from './compareVersion';
-export * from './createAsyncReadonlyStore';
 export * from './createAsyncStore';
-export * from './createReadonlyStore';
 export * from './createStore';
 export { createStore as default } from './createStore';
-export * from './errors';
 export * from './types';
