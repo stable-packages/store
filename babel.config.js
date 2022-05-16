a02d945be2eb83780b01e3e9ddeffcd7abddb90c
unional <homawong@gmail.com>
2022-05-15 17:21:03 -0700

    chroe: use depcheck adn restore husky 4
    Something is wrong that husky is not working.
    Need to take a look

--- babel.config.js
+++ babel.config.js
@@ -11,10 +11,5 @@ module.exports = {
     ['@babel/preset-typescript', {
       allowNamespaces: true
     }],
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator'
-  ],
+  ]
 };
