eb8fb97bff4b16d91704bd695760b80b88593ebc
Homa Wong <homawong@gmail.com>
2022-08-15 13:37:17 -0700

    chore: update demo

--- ts/types.ts
+++ ts/types.ts
@@ -7,6 +7,10 @@ export type StoreInitializer<T extends StoreValue> = (current: StoreValue, proce
 export interface StoreOptions<T extends StoreValue> {
   moduleName: string,
   key?: string,
+  /**
+   * The store version.
+   * It can be `major.minor.patch` (recommended) or number.
+   */
   version: StoreVersion,
   initializer: StoreInitializer<T>
 }
